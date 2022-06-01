f17b5e26af9fe89abda66c702bc079070c1a35c1
robs-am <roberta.amaro89@gmail.com>
2022-06-01 19:29:14 -0300

    finalizacao js com datepicker

--- js/main.js
+++ js/main.js
@@ -1,41 +1,62 @@
 //API key
 //https://api.nasa.gov/planetary/apod?api_key=jgGpw1ytsSROTdgYpdxxft56s9DDK0GI5PQflFUy
 
-const baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
-const apiKey = "gtAoVhMXRWgNSJBwefTrxOhwmzb8QjUHY0pEehFE";
 
-function fetchData() {
-  try {
-    fetch(baseUrl + apiKey)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
-      })
-      console.log(error)
-    displaydata(json)
+function nasarequested(){
+  const baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
+ const apiKey = "rBKlXGLOi8PziVei7flTrUGT0GYIhnQ0XPhRBUlM";
+ const dateInput = document.querySelector("#datepicker");
+          dateInput.addEventListener('change',(e)=>{
+            e.preventDefault();
+            nasarequested();
+          })
+ const title = document.querySelector("#title");
+ const copyright = document.querySelector("#copyright");
+ const mediaSection = document.querySelector("#media-section");
+ const information = document.querySelector("#description");
+
+ const currentDate =new Date().toISOString().slice(0, 10);
+
+
+ const imageSection =`<a id="hdimg" href="" target="-blank">
+  <div class="image-div">
+  <img id="image_of_the_day" src="" alt="image-by-nasa">
+  </div>
+ </a>`
+
+ const videoSection=`<div class="video-div"> <iframe id="videoLink" src="" frameborder="0"></iframe></div>`
+
+ let newDate = "&date="+dateInput.value+"&";
+
+
+ function fetchData(){
+  try{
+  fetch(baseUrl+apiKey+newDate)
+  .then(response=> response.json())
+  .then(json=>{
+  console.log(json);
+  diplaydata(json)
+  })
   }catch(error){
-    console.log(error)
+  console.log(error)
   }
-}
+  }
 
-fetchData()
+ function diplaydata(data){
 
-const title = document.querySelector("#title");
-const copyright = document.querySelector("#copyright");
-const mediaSection = document.querySelector("#media-section");
-const information = document.querySelector("#description");
-
-function displaydata(data){
   title.innerHTML=data.title;
-}
 
-if(data.hasOwnProperty("copyright")){
+  if(data.hasOwnProperty("copyright")){
   copyright.innerHTML=data.copyright;
   } else{
   copyright.innerHTML=""
   } 
 
-if(data.media_type=="video"){
+  date.innerHTML=data.date;
+  dateInput.max=currentDate;
+  dateInput.min="1995-06-16";
+
+  if(data.media_type=="video"){
   mediaSection.innerHTML=videoSection;
   document.getElementById("videoLink").src=data.url;
 
@@ -44,11 +65,9 @@ if(data.media_type=="video"){
   document.getElementById("hdimg").href=data.hdurl;
   document.getElementById("image_of_the_day").src=data.url;
   }
+  information.innerHTML=data.explanation
+ }
+ fetchData();
+}
 
-  const imageSection =`<a id="hdimg" href="" target="-blank">
-  <div class="image-div">
-  <img id="image_of_the_day" src="" alt="image-by-nasa">
-  </div>
-  </a>`
-
-const videoSection=`<div class="video-div"> <iframe id="videoLink" src="" frameborder="0"></iframe></div>`
+nasarequested().onload;
