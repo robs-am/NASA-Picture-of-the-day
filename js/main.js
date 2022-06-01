a1a214f194aa78eddad1912017e45657443a80a6
robs-am <roberta.amaro89@gmail.com>
2022-06-01 14:18:57 -0300

    add consts

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 //API key
 //https://api.nasa.gov/planetary/apod?api_key=jgGpw1ytsSROTdgYpdxxft56s9DDK0GI5PQflFUy
 
-function fetchData(){
-    try{
-      fetch(baseUrl+apiKey)
-      .then(response=>response.json())
-      .then(json=>{
+function fetchData() {
+  try {
+    fetch(baseUrl + apiKey)
+      .then(response => response.json())
+      .then(json => {
         console.log(json)
       })
-    }catch(error){
-      console.log(error)
-    }
+  } catch (error) {
+    console.log(error)
   }
-  fetchData()
+}
+fetchData()
+
+const title = document.querySelector("#title");
+const copyright = document.querySelector("#copyright");
+const mediaSection = document.querySelector("#media-section");
+const information = document.querySelector("#description");
