e04f77b66c5f783bead1fc9a1adcbbd2c2d1a73b
robs-am <roberta.amaro89@gmail.com>
2022-06-01 14:30:57 -0300

    api key

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //API key
 //https://api.nasa.gov/planetary/apod?api_key=jgGpw1ytsSROTdgYpdxxft56s9DDK0GI5PQflFUy
 
+const baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
+const apiKey = "gtAoVhMXRWgNSJBwefTrxOhwmzb8QjUHY0pEehFE";
+
 function fetchData() {
   try {
     fetch(baseUrl + apiKey)
@@ -8,10 +11,13 @@ function fetchData() {
       .then(json => {
         console.log(json)
       })
-  } catch (error) {
+      console.log(error)
+    displaydata(json)
+  }catch(error){
     console.log(error)
   }
 }
+
 fetchData()
 
 const title = document.querySelector("#title");
@@ -20,4 +26,29 @@ const mediaSection = document.querySelector("#media-section");
 const information = document.querySelector("#description");
 
 function displaydata(data){
+  title.innerHTML=data.title;
 }
+
+if(data.hasOwnProperty("copyright")){
+  copyright.innerHTML=data.copyright;
+  } else{
+  copyright.innerHTML=""
+  } 
+
+if(data.media_type=="video"){
+  mediaSection.innerHTML=videoSection;
+  document.getElementById("videoLink").src=data.url;
+
+  }else{
+  mediaSection.innerHTML=imageSection;
+  document.getElementById("hdimg").href=data.hdurl;
+  document.getElementById("image_of_the_day").src=data.url;
+  }
+
+  const imageSection =`<a id="hdimg" href="" target="-blank">
+  <div class="image-div">
+  <img id="image_of_the_day" src="" alt="image-by-nasa">
+  </div>
+  </a>`
+
+const videoSection=`<div class="video-div"> <iframe id="videoLink" src="" frameborder="0"></iframe></div>`
